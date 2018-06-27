c22e57d0ec42da7722a2f54290fcd6fad742a151
Chris Schofield <chris.schofield@jaystack.com>
2018-06-27 12:21:24 +0200

    Basic implementation of NeDB + Master Singleton

--- src/renderer/side-effects/NeDB.js
+++ src/renderer/side-effects/NeDB.js
@@ -1,1 +1,27 @@
-export default {}; // here comes the NeDB instance
+import NeDB from 'nedb';
+import {join} from "path";
+import remote from 'electron';
+import fs from 'fs';
+
+const exists = path => fs.exist(path);
+export const homeDir = join(remote.app.getPath('home'), '.console');
+export const dbName = name => join(homeDir,`db/${name}.db`);
+
+if (!exists(join(homeDir,'db'))) {
+  fs.mkdirSync(join(homeDir,'db'))
+}
+
+export default class DbManager {
+  static instance;
+
+  constructor() {
+    if (this.constructor.instance) return this.constructor.instance;
+    this.dbs = {};
+    this.createStores();
+    this.constructor.instance = this;
+  }
+
+  createStores() {
+    this.dbs.slack = { conversations: new NeDB({ filename: dbName('slack.conversations') }) };
+  }
+}
