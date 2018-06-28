34514e155f8080acf4510909af535397b950087b
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2018-06-28 10:13:39 +0200

    add menu

--- src/renderer/components/App.js
+++ src/renderer/components/App.js
@@ -4,17 +4,36 @@ import { init } from '../actions';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MoreVert from '@material-ui/icons/MoreVert';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 import Config from './Config';
 import Search from './Search';
 import Timeline from './Timeline';
 
 @connect(null, { init })
 export default class extends React.PureComponent {
+  state = {
+    anchorEl: null
+  };
+
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
   async componentDidMount() {
     await this.props.init();
   }
 
   render() {
+    const { anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+
     return (
       <div>
         <AppBar position="static" color="default">
@@ -23,6 +42,32 @@ export default class extends React.PureComponent {
               Console_
             </Typography>
             <Search />
+            <div>
+              <IconButton
+                aria-owns={open ? 'menu-appbar' : null}
+                aria-haspopup="true"
+                onClick={this.handleMenu}
+                color="inherit"
+              >
+                <MoreVert />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right'
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right'
+                }}
+                open={open}
+                onClose={this.handleClose}
+              >
+                <MenuItem onClick={this.handleClose}>Settings</MenuItem>
+              </Menu>
+            </div>
           </Toolbar>
         </AppBar>
         <Config />
