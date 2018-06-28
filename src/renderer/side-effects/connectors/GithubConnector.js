615f4eed2281ec97caefcf61730461b538298fe6
Chris Schofield <chris.schofield@jaystack.com>
2018-06-28 09:58:03 +0200

    Improved class structure for connectors

--- src/renderer/side-effects/connectors/GithubConnector.js
+++ src/renderer/side-effects/connectors/GithubConnector.js
@@ -7,15 +7,16 @@ const db = new DbManager();
 export default class GithubConnector extends BaseConnector {
   async init(options) {
     await super.init(options, ' https://api.github.com/');
-    this.request('get', 'users/jaystack/repos')
-      .then((resp) => {
-        db.dbs.github.repos.upsertAll(resp.map(GithubRepoTransformer))
-          .then(() => console.log('LOADED GITHUB REPOS'))
-          .catch(err => console.error(err));
-        db.dbs.github.repos.find()
+    this.fetchRepos('jaystack');
+  }
+
+  fetchRepos(user) {
+    this.request('get', `users/${user}/repos`)
+      .then((resp) => db.dbs.github.repos.upsertAll(resp.map(GithubRepoTransformer))
+        .then(() => db.dbs.github.repos.find()
           .then((res => console.log('TOTAL GITHUB REPOS', res)))
-          .catch(e => console.error(e));
-      })
+          .catch(e => console.error(e)))
+        .catch(err => console.error(err)))
       .catch(err => console.error(err));
   }
 }
--- src/renderer/side-effects/connectors/SlackConnector.js
+++ src/renderer/side-effects/connectors/SlackConnector.js
@@ -7,13 +7,16 @@ const db = new DbManager();
 export default class SlackConnector extends BaseConnector {
   async init(options) {
     await super.init(options, 'http://slack.com/api/');
+    this.fetchConversations();
+  }
+
+  fetchConversations() {
     this.request('get', 'conversations.list')
       .then((resp) => {
         db.dbs.slack.conversations.upsertAll(resp.channels.map(SlackConversationTransformer))
-          .then(() => console.log('LOADED SLACK CHANNELS'))
-          .catch(err => console.error(err));
-        db.dbs.slack.conversations.find()
-          .then(res => console.log('SLACK CHANNELS TOTAL', res))
+          .then(() => db.dbs.slack.conversations.find()
+            .then(res => console.log('SLACK CHANNELS TOTAL', res))
+            .catch(err => console.error(err)))
           .catch(err => console.error(err));
       })
       .catch(err => console.error(err));
