3c9661dc85364885db3d45a5ed4b43b18f81a73e
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2018-06-29 11:10:03 +0200

    refactor actions and selectors

--- src/renderer/selectors.js
+++ src/renderer/selectors.js
@@ -1,10 +1,5 @@
 export const getConfig = state => state.config;
 
-export const getConfigOfSource = type => state => {
-  const config = getConfig(state);
-  return config.sources.find(subConfig => subConfig.type === type);
-};
-
 export const getQuery = state => state.query;
 
 export const getItems = state => state.items;
