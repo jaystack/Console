85256a071858d0ddce76b7b5f058b6620d99d65e
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2018-06-29 15:22:50 +0200

    add fromNow information to timeline dates

--- src/renderer/components/Item.js
+++ src/renderer/components/Item.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import moment from 'moment';
 
 export default class extends React.PureComponent {
   render() {
     const { item } = this.props;
+    const date = new Date(item.created * 1000);
     return (
       <Card className="item">
         <CardContent>
@@ -13,7 +15,9 @@ export default class extends React.PureComponent {
           <Typography variant="headline" component="h2">
             {item.user}
           </Typography>
-          <Typography color="textSecondary">{new Date(item.created * 1000).toLocaleString()}</Typography>
+          <Typography color="textSecondary">
+            <b>{moment(date).fromNow()}</b> - {date.toLocaleString()}
+          </Typography>
           <Typography component="p">{item.content}</Typography>
         </CardContent>
       </Card>
