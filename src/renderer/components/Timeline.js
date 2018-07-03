d6c32e81f6110b015968150c0dbc834f3858f1b5
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2018-07-03 13:01:30 +0200

    infinite scroll in timeline

--- src/renderer/components/Timeline.js
+++ src/renderer/components/Timeline.js
@@ -5,8 +5,23 @@ import Item from './Item';
 
 @connect(state => ({ items: getItems(state) }))
 export default class extends React.PureComponent {
+  state = {
+    max: 25
+  };
+
+  handleScroll = evt => {
+    if (evt.target.scrollTop >= evt.target.scrollHeight - evt.target.offsetHeight - 100) {
+      this.setState({ max: this.state.max + 25 });
+    }
+  };
+
   render() {
     const { items } = this.props;
-    return <div className="timeline">{items.map(item => <Item key={item.id} item={item} />)}</div>;
+    const { max } = this.state;
+    return (
+      <div className="timeline" onScroll={this.handleScroll}>
+        {items.slice(0, max).map(item => <Item key={item.id} item={item} />)}
+      </div>
+    );
   }
 }
