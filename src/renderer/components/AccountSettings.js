ce9749c93d5e5f2425e18cb6a2946b6baa543c99
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2018-07-11 14:25:12 +0200

    wip display accounts

--- src/renderer/components/AccountSettings.js
+++ src/renderer/components/AccountSettings.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import AddAccountDialog from './AddAccountDialog';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { getAccounts } from '../selectors';
 
-@connect()
+@connect(state => ({ accounts: getAccounts(state) }))
 export default class extends React.PureComponent {
   state = {
     isOpenAddAccountDialog: false
@@ -14,6 +19,7 @@ export default class extends React.PureComponent {
   handleAddAccountDialogClose = () => this.setState({ isOpenAddAccountDialog: false });
 
   render() {
+    const { accounts } = this.props;
     const { isOpenAddAccountDialog } = this.state;
     return (
       <div className="account-settings">
@@ -23,7 +29,42 @@ export default class extends React.PureComponent {
           </Button>
         </header>
         <AddAccountDialog open={isOpenAddAccountDialog} onClose={this.handleAddAccountDialogClose} />
+        <main>{accounts.map(account => <Account key={account._id} {...account} />)}</main>
       </div>
+    );
+  }
+}
+
+class Account extends React.PureComponent {
+  getImg() {
+    switch (this.props.type) {
+      case 'slack':
+        return 'static/slack-logo.png';
+      case 'github':
+        return 'static/github-logo.png';
+      case 'email':
+        return 'static/email-logo.png';
+      default:
+        return '';
+    }
+  }
+
+  render() {
+    const { name } = this.props;
+    return (
+      <Paper classes={{ root: 'account-tile' }}>
+        <div className="img-container">
+          <img alt="account-type" src={this.getImg()} />
+        </div>
+        <div className="name-container">
+          <Typography variant="display1">{name}</Typography>
+        </div>
+        <div className="button-container">
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+        </div>
+      </Paper>
     );
   }
 }
