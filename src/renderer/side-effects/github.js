21622f94657fbd4d00c050c37e9b36522895f887
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2018-07-19 12:37:11 +0200

    github api queue

--- src/renderer/side-effects/github.js
+++ src/renderer/side-effects/github.js
@@ -1,4 +1,4 @@
-import invoke from './request';
+import coreInvoke from './request';
 import { sleep } from './time';
 
 const baseUrl = 'https://api.github.com';
@@ -11,24 +11,68 @@ const getNext = links => {
   }, null);
 };
 
-const request = async (token, method, url, { headers = {}, query = {}, prevResults, isNext = false, ...args } = {}) => {
-  const response = await invoke(method, isNext ? url : baseUrl + url, {
-    headers: { ...headers, Authorization: `Bearer ${token}` },
-    query: isNext ? {} : { ...query, per_page: 100 },
-    ...args
-  });
-  const result = prevResults ? [ ...prevResults, ...response.data ] : response.data;
-  const next = getNext(response.headers.link);
-  if (next) {
-    await sleep(1000);
-    return await request(token, method, next, { isNext: true, prevResults: result });
-  } else {
-    return result;
-  }
-};
+export default () => {
+  const queues = new Map();
 
-export const resolveAccountByToken = async token => {
-  const { id, login: username } = await request(token, 'get', '/user');
-  const repos = await request(token, 'get', '/user/repos');
-  return { id, username, repos: repos.map(({ id, name, owner: { login: owner } }) => ({ id, name, owner })) };
+  const addToQueue = request => {
+    return new Promise((resolve, reject) => {
+      const callback = (error, result) => (error ? reject(error) : resolve(result));
+      if (queues.has(request.token)) queues.get(request.token).requests.push({ ...request, callback });
+      else queues.set(request.token, { requests: [ { ...request, callback } ], inProgress: false });
+      trigger(queues.get(request.token));
+    });
+  };
+
+  const trigger = async queue => {
+    if (queue.inProgress) return;
+    const request = queue.requests.shift();
+    if (!request) return;
+    queue.inProgress = true;
+    await fetch(request);
+    await sleep(1000);
+    queue.inProgress = false;
+    trigger(queue);
+  };
+
+  const fetch = async ({ token, method, url, headers, query, body, callback }) => {
+    try {
+      const response = await coreInvoke(method, url, {
+        headers: { ...headers, Authorization: `Bearer ${token}` },
+        query,
+        body
+      });
+      callback(null, response);
+    } catch (error) {
+      callback(error);
+    }
+  };
+
+  const invoke = async (token, method, url, { headers = {}, query = {}, body, prevResults, isNext = false } = {}) => {
+    const response = await addToQueue({
+      token,
+      method,
+      url: isNext ? url : baseUrl + url,
+      headers: { ...headers, Authorization: `Bearer ${token}` },
+      query: isNext ? {} : { ...query, per_page: 100 },
+      body
+    });
+    const result = prevResults ? [ ...prevResults, ...response.data ] : response.data;
+    const next = getNext(response.headers.link);
+    if (next) {
+      await sleep(1000);
+      return await invoke(token, method, next, { isNext: true, prevResults: result });
+    } else {
+      return result;
+    }
+  };
+
+  const resolveAccountByToken = async token => {
+    const { id, login: username } = await invoke(token, 'get', '/user');
+    const repos = await invoke(token, 'get', '/user/repos');
+    return { id, username, repos: repos.map(({ id, name, owner: { login: owner } }) => ({ id, name, owner })) };
+  };
+
+  return {
+    resolveAccountByToken
+  };
 };
--- src/renderer/store.js
+++ src/renderer/store.js
@@ -2,14 +2,14 @@ import Store, { thunk } from 'repatch';
 import * as config from './side-effects/config';
 import * as time from './side-effects/time';
 import * as slack from './side-effects/slack';
-import * as github from './side-effects/github';
+import github from './side-effects/github';
 import ConnectorManager from './side-effects/connectors/ConnectorManager';
 import NeDB from './side-effects/NeDB';
 
 const initialState = {
   config: null,
   isFetching: true,
-  activeSettingsMenuItem: 'projects',
+  activeSettingsMenuItem: 'accounts',
   accounts: [],
   projects: [],
   selectedProjectId: null,
@@ -17,10 +17,8 @@ const initialState = {
   query: ''
 };
 
-const db = new NeDB();
-const connectors = new ConnectorManager();
 const store = new Store(initialState).addMiddleware(
-  thunk.withExtraArgument({ config, connectors, db, time, slack, github })
+  thunk.withExtraArgument({ config, connectors: new ConnectorManager(), db: new NeDB(), time, slack, github: github() })
 );
 
 window.store = store;
